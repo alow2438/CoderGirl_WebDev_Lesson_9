74da5a980d2f529cda4cbfd09d2eef7f95de5dd0
Aurelia <aurelialowther@gmail.com>
2019-03-31 11:30:01 -0500

    Code is working for everything but guessing the word

--- wheel_of_fortune.js
+++ wheel_of_fortune.js
@@ -1,45 +1,28 @@
 // wheel_of_fortune.js
 // CoderGirl WebDev (Winter 2019) — LaunchCode
 
-// VARIABLES /////////////////////////////////////////////////////////
 
-// In a normal application you would ask for user input.
-// In this exercise we will set the variables ourselves.
+var word = 'DOG'; 
+var isGuessingLetter = true; 
+var letter = 'A'; 
 
-// Set a secret word!
+if (isGuessingLetter === true){
+	switch (letter) {
+		case "D":
+			console.log(letter+" _ _"); 
+			break;
+		case "O":
+			console.log("_ " +letter+ " _"); 
+			break;
+		case "G":
+			console.log("_ _ " +letter); 
+			break;	
+		default:
+			console.log("Sorry, no " + letter+ "!");	
+	}
+}else{
+	if (word = "DOG"){ 
+		console.log("Congratulations! You won Wheel of Fortune!"); 
 
-var word = 'ANSWER'; // ALL CAPS
-
-// Ask player if they would like to guess a letter or guess the
-// solution.
-
-var isGuessingLetter = true; // set to false to guess solution
-
-// If they are guessing a letter, set the letter that they are
-// guessing.
-
-var letter = 'A'; // UPPERCASE
-
-// SCRIPT ////////////////////////////////////////////////////////////
-
-// If guessing a letter...
-
-	// If the word contains the letter...
-
-		// Print back the word with all instances of that letter revealed.
-
-	// If the word does not contain the letter...
-
-		// Print back "Sorry, no <letter>!" but with <letter> replaced by the player's guess.
-
-// If guessing the word...
-
-	// If they are correct...
-
-		// Print "Congratulations! You won Wheel Of Fortune!"
-
-	// If they are incorrect...
-
-		// Print back "Nope, keep trying!"
-
-// End of script!
+	}else console.log("Nope, keep trying!"); 
+}
